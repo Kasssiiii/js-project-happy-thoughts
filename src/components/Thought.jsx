9fef2a2376c42758b809f91ae02f0cd9081bff8a
Kasssiiii <kasia.gru@gmail.com>
2025-06-05 16:44:36 +0200

    format posts based on sampler. added moment.js

--- src/components/Thought.jsx
+++ src/components/Thought.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import moment from "moment";
 
 import "./Thought.css";
 
-export const Thought = () => {
+export const Thought = ({ post }) => {
+  console.log(post);
   return (
     <div className="postBox">
-      <p> Thought </p>
+      <p> {post.message} </p>
       <div className="details">
         <div>
-          <button className="like">❤️</button> x10
+          <button className="like">❤️</button> x{post.hearts}
         </div>
-        <div> sent .. X min ago</div>
+        <div> sent {moment(post.createdAt).fromNow()}</div>
       </div>
     </div>
   );
--- src/components/Thoughts.jsx
+++ src/components/Thoughts.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Thought } from './Thought';
+import React from "react";
+import { Thought } from "./Thought";
 
 export const Thoughts = () => {
-    return (
-        <>
-        <Thought />
-        <Thought />
-        <Thought />
-        <Thought />
-        </>
-    )
-}
+  const postSample = [
+    {
+      _id: "5dd671c864cc480017f40979",
+      message: "I'm happy because we're starting a fun new project",
+      hearts: 22,
+      createdAt: "2019-11-21T11:15:20.888Z",
+      __v: 0,
+    },
+    {
+      _id: "5dd6759064cc480017f4097a",
+      message: "I just ate a super tasty lunch",
+      hearts: 5,
+      createdAt: "2019-11-21T11:31:28.547Z",
+      __v: 0,
+    },
+  ];
+
+  return (
+    <>
+      {postSample.map((post) => (
+        <Thought key={post._id} post={post} />
+      ))}
+    </>
+  );
+};
