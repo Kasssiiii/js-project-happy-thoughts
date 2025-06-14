507350da4db33606cfae5faa2c235a72eec22b38
Kasssiiii <kasia.gru@gmail.com>
2025-06-14 12:53:08 +0200

    api for sending and liking post

--- src/thoughtsAPI.js
+++ src/thoughtsAPI.js
@@ -1,13 +1,20 @@
 const APIUri = "https://happy-thoughts-api-4ful.onrender.com";
 
-const getResponse = async (path, data, response) => {
+const getResponse = async (path, params, body, response) => {
   try {
-    const resp = await fetch(`${APIUri}/${path}`, data ? { body: data } : {});
+    if (body) {
+      params.body = JSON.stringify(body);
+      params.headers = {
+        "Content-Type": "application/json",
+      };
+    }
+    const resp = await fetch(`${APIUri}/${path}`, params);
     if (resp.ok) {
-      data = await resp.json();
+      const data = await resp.json();
       response(200, data);
     } else {
-      response(resp.status, { error: "Server error" });
+      const data = await resp.json();
+      response(resp.status, { error: data });
     }
   } catch (error) {
     response(500, { error: error });
@@ -15,5 +22,22 @@ const getResponse = async (path, data, response) => {
 };
 
 export const getLatest = (response) => {
-  getResponse("thoughts", null, response);
+  getResponse("thoughts", {}, null, response);
+};
+
+export const sendPost = (text, response) => {
+  const body = {
+    message: text,
+  };
+  const params = {
+    method: "POST",
+  };
+  getResponse("thoughts", params, body, response);
+};
+
+export const likePost = (id, response) => {
+  const params = {
+    method: "POST",
+  };
+  getResponse(`thoughts/${id}/like`, params, null, response);
 };
