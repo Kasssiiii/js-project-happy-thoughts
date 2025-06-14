ee7e6ad82de68910f8b54227e10ccfa8550b93d5
Kasssiiii <kasia.gru@gmail.com>
2025-06-14 12:56:16 +0200

    liking thoughts

--- src/components/Thought.jsx
+++ src/components/Thought.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 
 import "./Thought.css";
+import { likePost } from "../thoughtsAPI";
 
 export const Thought = ({ post }) => {
+  const [tick, tock] = useState(false);
+
+  const like = () => {
+    likePost(post._id, (code, data) => {
+      if (code === 200) {
+        post.hearts = data.hearts;
+        tock(!tick);
+      }
+    });
+  };
   return (
     <div className="postBox">
       <p> {post.message} </p>
       <div className="details">
         <div>
-          <button className="like">❤️</button> x{post.hearts}
+          <button className="like" onClick={like}>
+            ❤️
+          </button>{" "}
+          x{post.hearts}
         </div>
         <div> sent {moment(post.createdAt).fromNow()}</div>
       </div>
